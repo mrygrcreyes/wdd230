aa7b64fc3b46695969e94f5be3c64b74d7461e06
Mary Grace <marygracereyes2315@gmail.com>
2024-01-13 18:56:02 +0800

    Edit Home Page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -10,3 +10,23 @@ var lastModified = document.lastModified;
 
 // Set element with id lastModified to last modified date
 document.getElementById("lastModified").innerHTML = 'Last Modification: ' + lastModified;
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
