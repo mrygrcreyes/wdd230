7c6526b32d3accad6347fcbf7d88d3ad65fb3dc2
Mary Grace <marygracereyes2315@gmail.com>
2024-02-03 00:43:30 +0800

    Edit Form

---
+++ scripts/form.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const isPasswdSameMsg = document.querySelector(".is-passwd-same");
+const passwd = document.getElementById('password');
+const confirmPasswd = document.getElementById('confirm-passwd');
+
+
+const isPasswdConfirmed = function () {
+    if (passwd.value !== confirmPasswd.value) {
+        const message = `❗️WRONG PASSWORD`
+        isPasswdSameMsg.textContent = message;
+        confirmPasswd.classList.add("notsame");
+    } else {
+        isPasswdSameMsg.textContent = "";
+        confirmPasswd.classList.remove("notsame");
+    }
+}
+
+confirmPasswd.addEventListener('focusout', isPasswdConfirmed);
+
+// const rangevalue = document.getElementById("rangevalue");
+// const range = document.getElementById("page-rating");
+
+// // RANGE event listener
+// range.addEventListener('change', displayRatingValue);
+// range.addEventListener('input', displayRatingValue);
+
